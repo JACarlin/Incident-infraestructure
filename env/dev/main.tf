2809741acbb06a6c18c27bb5e15cb0b0fbd3583c
JACarlin <alexcarlin02@outlook.com>
2024-10-21 09:29:54 -0600

    fix nginx

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -7,7 +7,7 @@ module "dev_vm" {
   vnet_name = "IN-VNET-Carlin-Dev"
   subnet_name = "IN-SUBNET-Carlin-Dev"
   ip_name = "IN-IP-Carlin-Dev"
-  location = "eastus2"
+  location = "eastus"
   admin_username = "adminuser"
   nic_name = "IN-NIC-Carlin-Dev"
   vm_name = "IN-Server-Carlin-Dev"
